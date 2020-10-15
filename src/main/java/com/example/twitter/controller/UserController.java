1ee0c2afd47521bd0c835706018e082b617f8cc0
wenqing <w285liu@uwaterloo.ca>
2020-10-15 11:19:10 -0400

    use JdbcTemplate to get all

--- src/main/java/com/example/twitter/controller/UserController.java
+++ src/main/java/com/example/twitter/controller/UserController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.dao.DataAccessException;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import java.sql.*;
 
@@ -16,12 +21,14 @@ import com.example.twitter.models.*;
 @Controller
 @RequestMapping(path="/demo")
 public class UserController {
-	 static final String JDBC_DRIVER = "com.example.twitter.jdbc.Driver";  
-	 static final String DB_URL = "jdbc:mysql://18.224.93.66/DBTEST";
+	@Autowired
+    JdbcTemplate jdbcTemplate;    	
+    static final String JDBC_DRIVER = "com.example.twitter.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://18.224.93.66/DBTEST";
 
 	 //  Database credentials
-	 static final String USER = "remote";
-	 static final String PASS = "657@SJTUw";
+    static final String USER = "remote";
+    static final String PASS = "657@SJTUw";
 	
     @GetMapping(path="/test")
     public @ResponseBody String test() {
@@ -67,5 +74,30 @@ public class UserController {
      	    }//end finally try
        }//end try
        return a;
+    }
+    
+    @GetMapping(path="/getAllTest2")
+    public @ResponseBody List<User> getAll2(){
+    	ArrayList<User> result = new ArrayList<>();
+    	try {
+    	    List<Map<String,Object>> r = jdbcTemplate.queryForList("SELECT * FROM user");
+    	    for(int i=0;i<r.size();i++) {
+    	    	Map<String,Object> m = r.get(i);
+    	    	String username = m.get("username").toString();
+    	    	String password = m.get("password").toString();
+    	    	result.add(new User(username,password));
+    	    }
+    	    return result;
+    	}
+    	catch(DataAccessException e) {
+    		e.printStackTrace();
+    	}
+    	catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    	finally {
+    		
+    	}
+    	return result;
     }
 }
