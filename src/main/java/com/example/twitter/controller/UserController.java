41db5b5448d50aacb865941e48bcb0dd52a50abd
wenqing <w285liu@uwaterloo.ca>
2020-10-16 15:16:41 -0400

    use JdbcTemplate to crud

--- src/main/java/com/example/twitter/controller/UserController.java
+++ src/main/java/com/example/twitter/controller/UserController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.dao.DataAccessException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
-import java.sql.*;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import com.example.twitter.models.*;
 
 @Controller
@@ -23,71 +23,13 @@ import com.example.twitter.models.*;
 public class UserController {
 	@Autowired
     JdbcTemplate jdbcTemplate;    	
-    static final String JDBC_DRIVER = "com.example.twitter.jdbc.Driver";  
-	static final String DB_URL = "jdbc:mysql://18.224.93.66/DBTEST";
-
-	 //  Database credentials
-    static final String USER = "remote";
-    static final String PASS = "657@SJTUw";
-	
-    @GetMapping(path="/test")
-    public @ResponseBody String test() {
-    	return "hello world!";
-    }
-    @GetMapping(path="/getAllUser")
-    public @ResponseBody ArrayList<User> getAll(){
-    	Connection conn = null;
-    	Statement stmt = null;
-    	ArrayList<User> a = new ArrayList<User>();
-     	try {
-    	    conn = DriverManager.getConnection(DB_URL,USER,PASS);
-    	    stmt = conn.createStatement();
-    	    String sql;
-    	    sql = "SELECT * FROM user";
-    	    ResultSet rs = stmt.executeQuery(sql);
-    	    while(rs.next()) {
-    	    	a.add(new User(rs.getString("username"),rs.getString("password")));
-    	    }
-    	    rs.close();
-    	    stmt.close();
-    	    conn.close();
-    	    System.out.println(a);
-    	    return a;
-    	}
-    	catch(SQLException se) {
-    		se.printStackTrace();
-    	}
-     	catch(Exception e) {
-     	    e.printStackTrace();	
-     	}
-     	finally {
-     		try{
-     	      if(stmt!=null)
-     	        stmt.close();
-     	    }catch(SQLException se2){
-     	    }// nothing we can do
-     	    try{
-     	       if(conn!=null)
-     	          conn.close();
-     	    }catch(SQLException se){
-     	       se.printStackTrace();
-     	    }//end finally try
-       }//end try
-       return a;
-    }
     
-    @GetMapping(path="/getAllTest2")
-    public @ResponseBody List<User> getAll2(){
-    	ArrayList<User> result = new ArrayList<>();
+    @GetMapping(path="/getAllUsers")
+    public @ResponseBody List<User> getAllUser(){
+    	//List<User> result = new ArrayList<>();
     	try {
-    	    List<Map<String,Object>> r = jdbcTemplate.queryForList("SELECT * FROM user");
-    	    for(int i=0;i<r.size();i++) {
-    	    	Map<String,Object> m = r.get(i);
-    	    	String username = m.get("username").toString();
-    	    	String password = m.get("password").toString();
-    	    	result.add(new User(username,password));
-    	    }
-    	    return result;
+    		List<User> r = jdbcTemplate.query("SELECT * FROM user", new Object[]{}, new UserMapper());
+    	    return r;
     	}
     	catch(DataAccessException e) {
     		e.printStackTrace();
@@ -96,8 +38,15 @@ public class UserController {
     		e.printStackTrace();
     	}
     	finally {
-    		
     	}
-    	return result;
+    	System.out.println("hello");
+    	return new ArrayList<User>();
     }
+}
+
+class UserMapper implements RowMapper<User>{
+	@Override
+	public User mapRow(ResultSet rs, int numRow) throws SQLException {
+		return new User(rs.getString("username"),rs.getString("password"));
+	}
 }
